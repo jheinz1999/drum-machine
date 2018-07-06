026ddc6729d024f99db881b28054f1ed449f8ca4
Jonathan Heinz <jonathan.heinz@aol.com>
2018-07-06 02:34:47 -0700

    Sampling now works

---
+++ drum-machine/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get("/samples/:sample", function(req, res) {
+
+  res.sendFile(__dirname + "/src/samples/" + req.params.sample);
+
+});
+
+app.listen(4000);
+
+console.log("Running");
